258164a2f4be6289c8d5d46a72de6d9cde79958d
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2023-07-08 13:07:59 +0530

    first commit

--- pomodoro/pomodoroApp.swift
+++ pomodoro/pomodoroApp.swift
@@ -14,7 +14,6 @@ struct pomodoroApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
